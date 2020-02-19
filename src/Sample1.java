48eae2029a2466492e06eb1db5c49c08d87bbdb9
lllverb <54735495+lllverb@users.noreply.github.com>
2020-02-19 17:29:49 +0900

    マタンゴ出現

--- src/Sample1.java
+++ src/Sample1.java
@@ -4,6 +4,15 @@ public class Sample1 {
 		h.name = "ミナト";
 		h.hp = 100;
 		System.out.println("勇者" + h.name + "を生み出しました!");
+		
+		Matango m1 = new Matango();
+		m1.hp = 50;
+		m1.suffix = 'A';
+		System.out.println("マタンゴ" + m1.suffix + "を生み出しました!");
+		Matango m2 = new Matango();
+		m2.hp = 48;
+		m2.suffix = 'B';
+		System.out.println("マタンゴ" + m2.suffix + "を生み出しました!");
 		h.sit(5);
 		h.slip();
 		h.sit(25);
